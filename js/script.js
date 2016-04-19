98216e5c42064ed2759e415f6b07ad9a432fdde9
Daniel M. Sanchez <dmsanchez86@misena.edu.co>
2016-04-19 18:17:23 -0500

    add meta tags

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 
 $().ready(function($) {
 	$('.containerMenuMobile span').unbind('click').click(function(event) {
-		$("nav ul").toggleClass('open');
+		$("nav ul").addClass('open');
+
+		$("nav ul li a").unbind('click').click(function(event) {
+			$("nav ul").removeClass('open');
+		});
 	});
 });
 
